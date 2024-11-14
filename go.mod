c2faa865398afa6f38cf3bc32f7bbb2c35cea8cd
hidu <duv123+git@gmail.com>
2024-11-14 15:11:39 +0800

    update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xanygo/webr
 
 go 1.23.2
+
+require github.com/xanygo/anygo v0.0.0-20241114063319-94b7a1b28ad3
--- zip.go
+++ zip.go
@@ -5,61 +5,20 @@
 package webr
 
 import (
-	"archive/zip"
-	"bytes"
-	"crypto/md5"
 	_ "embed"
-	"encoding/hex"
-	"fmt"
-	"io"
-	"io/fs"
-	"net/http"
-	"sync"
+
+	"github.com/xanygo/anygo/xhttp"
 )
 
 //go:embed asset/asset.zip
 var zipFile []byte
 
-func Handler() http.Handler {
-	return http.HandlerFunc(zipHandler)
-}
-
-func zipHandler(w http.ResponseWriter, req *http.Request) {
-	fileName := req.URL.Path
-	rd, err := zip.NewReader(bytes.NewReader(zipFile), int64(len(zipFile)))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+func Handler() *xhttp.ZipFile {
+	hd := &xhttp.ZipFile{
+		Content: zipFile,
 	}
-
-	if etag := fileMd5ETag(rd, fileName); etag != "" {
-		w.Header().Set("ETag", etag)
-		if match := req.Header.Get("If-None-Match"); match == etag {
-			w.WriteHeader(http.StatusNotModified)
-			return
-		}
+	if err := hd.Init(); err != nil {
+		panic(err)
 	}
-
-	http.ServeFileFS(w, req, rd, fileName)
-}
-
-var md5etag sync.Map
-
-func fileMd5ETag(fsSystem fs.FS, fp string) string {
-	if val, ok := md5etag.Load(fp); ok {
-		return val.(string)
-	}
-	file, err := fsSystem.Open(fp)
-	if err != nil {
-		return ""
-	}
-	defer file.Close()
-	m := md5.New()
-	if _, err = io.Copy(m, file); err != nil {
-		return ""
-	}
-	str := hex.EncodeToString(m.Sum(nil))
-	result := fmt.Sprintf("%q", str)
-	md5etag.Store(fp, result)
-	return result
+	return hd
 }
